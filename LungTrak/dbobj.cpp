680127dd9d7a782ec719c2e9185596b8cc0ae5cb
Iain <irc91914@gmail.com>
2020-07-08 13:03:40 +0100

    Add validation functions for rxr, nhs

--- LungTrak/dbobj.cpp
+++ LungTrak/dbobj.cpp
@@ -3,6 +3,7 @@
 #include <QMessageBox>
 #include <QSqlQuery>
 #include <QVariant>
+#include <QRegularExpression>
 
 DBObj::DBObj()
 {
@@ -59,8 +60,21 @@ int DBObj::db_getpatid(QString rxr, QString nhs)
 {
     QSqlQuery q1, q2;
 
+    if (!valid_rxr(rxr)) return -1;
+    else rxr = clean_rxr(rxr);
+
+    if (!valid_nhs(nhs)) nhs = "NULL";
+    else nhs = clean_nhs(nhs);
+
+    //debug
+    QMessageBox msg;
+    msg.setWindowTitle("NHS");
+    msg.setText(nhs);
+    msg.setIcon(QMessageBox::Information);
+    msg.exec();
+
     q1.prepare("SELECT id, nhs FROM pats WHERE rxr = ?");
-    q1.addBindValue(rxr.toUpper());
+    q1.addBindValue(rxr);
     q1.exec();
 
     if(q1.first()) {
@@ -74,7 +88,7 @@ int DBObj::db_getpatid(QString rxr, QString nhs)
     }
 
     q2.prepare("INSERT INTO pats (rxr, nhs) VALUES (?, ?)");
-    q2.addBindValue(rxr.toUpper());
+    q2.addBindValue(rxr);
     q2.addBindValue(nhs);
     q2.exec();
 
@@ -82,4 +96,44 @@ int DBObj::db_getpatid(QString rxr, QString nhs)
     if(q1.first()) return q1.value(0).toInt();
 
     return -1;
+}
+
+bool DBObj::valid_rxr(QString rxr)
+{
+    QRegularExpression re("\\d{7}");
+
+    rxr = rxr.trimmed();
+
+    if(rxr.length() != 10) return false;
+    if(!rxr.startsWith("rxr", Qt::CaseInsensitive)) return false;
+    if(re.match(rxr.right(7)).hasMatch()) return true;
+
+    return false;
+}
+
+bool DBObj::valid_nhs(QString nhs)
+{
+    QRegularExpression re("\\d{3}\\s*\\d{3}\\s*\\d{4}");
+
+    if(re.match(nhs.trimmed()).hasMatch()) return true;
+
+    return false;
+}
+
+QString DBObj::clean_rxr(QString rxr)
+{
+    return rxr.trimmed().toUpper();
+}
+
+QString DBObj::clean_nhs(QString nhs)
+{
+    QRegularExpression re("(\\d{3})\\s*(\\d{3})\\s*(\\d{4})");
+    QRegularExpressionMatch match;
+
+    match = re.match(nhs.trimmed());
+    if(match.hasMatch()) {
+        return (match.captured(1) + " " + match.captured(2) + " " + match.captured(3));
+    }
+
+    return "NULL";
 }
