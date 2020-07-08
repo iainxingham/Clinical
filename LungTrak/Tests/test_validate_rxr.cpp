5bc0c6ec964db5c0daf77901fabfeabc5f145a5c
Iain <irc91914@gmail.com>
2020-07-08 14:25:24 +0100

    Start of some tests

---
+++ LungTrak/Tests/test_validate_rxr.cpp
@@ -0,0 +1,19 @@
+#include <QtTest/QtTest>
+
+#include "dbobj.h"
+
+class test_validate : public QObject
+{
+    Q_OBJECT
+public:
+    test_validate() {}
+    virtual ~test_validate() {}
+
+private slots:
+    void test_validate_rxr();
+};
+
+void test_validate::test_validate_rxr()
+{
+
+}
