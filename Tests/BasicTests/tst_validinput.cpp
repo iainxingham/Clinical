2db2fcd4daf2056d22a436a4c102a01bd038b0d9
Iain <irc91914@gmail.com>
2020-07-08 11:43:12 +0100

    Prior to dealing with qmake issue

--- Tests/BasicTests/tst_validinput.cpp
+++ Tests/BasicTests/tst_validinput.cpp
@@ -15,10 +15,9 @@ public:
 private slots:
     void initTestCase();
     void cleanupTestCase();
-    void test_case1();
-
-    // Test valid_rxr() and valid_nhs()
-    void test_rxr_data();
+    void test_set_data();
+    void test_rxr_valid();
+    void test_nhs_valid();
 
 private:
     DBObj *db;
@@ -26,34 +25,56 @@ private:
 
 ValidInput::ValidInput()
 {
-    db = new DBObj();
+   // db = new DBObj();
 }
 
 ValidInput::~ValidInput()
 {
-    delete db;
+    //delete db;
 }
 
 void ValidInput::initTestCase()
 {
-
+    db = new DBObj();
 }
 
 void ValidInput::cleanupTestCase()
 {
-
+    delete db;
 }
 
-void ValidInput::test_case1()
+void ValidInput::test_set_data()
 {
+    QTest::addColumn<QString>("Input - rxr");
+    QTest::addColumn<QString>("Input - nhs");
+    QTest::addColumn<bool>("Result");
 
+    QTest::newRow("Leading whitespace") << "   RXR0000001" << "   111 222 3333" << true;
+    QTest::newRow("Trailing whitespace") << "RXR0000001    " << "111 222 3333    " << true;
+    QTest::newRow("Not digits") << "rxrabcdefg" << "aaa bbb cccc" << false;
+    QTest::newRow("Too few digits") << "RXR123456" << "11 222 3333" << false;
+    QTest::newRow("Weird whitespace") << " rxr1234567   " << "1112223333" << true;
+    QTest::newRow("Incorrect characters") << "xrx1234567" << "a111 222 3333" << false;
 }
 
-void ValidInput::test_rxr_data()
+void ValidInput::test_rxr_valid()
 {
+    QFETCH(QString, rxr);
+    QFETCH(QString, nhs);
+    QFETCH(bool, result);
 
+    QCOMPARE(db->valid_rxr(rxr), result);
+}
+
+void ValidInput::test_nhs_valid()
+{
+    QFETCH(QString, rxr);
+    QFETCH(QString, nhs);
+    QFETCH(bool, result);
+
+    QCOMPARE(db->valid_rxr(nhs), result);
 }
 
 QTEST_MAIN(ValidInput)
 
-//#include "tst_validinput.moc"
+#include "tst_validinput.moc"
